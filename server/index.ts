1bbe224987f1fdbaa483e51c1868b9f18eac6506
Lautaro Riveiro <a>
2019-07-08 14:30:12 -0300

    Reformat code

--- server/index.ts
+++ server/index.ts
@@ -1,30 +1,30 @@
-import { createServer } from 'http'
-import { parse } from 'url'
+import {createServer} from 'http'
+import {parse} from 'url'
 import next from 'next'
 import {join} from "path"
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({dev})
 const handle = app.getRequestHandler()
 const root = process.cwd()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    const { pathname } = parsedUrl
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url!, true)
+        const {pathname} = parsedUrl
 
-    if (pathname === '/appshell') {
-        return app.serveStatic(req, res, join(root, '/static/appshell.html'))
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(port)
+        if (pathname === '/appshell') {
+            return app.serveStatic(req, res, join(root, '/static/appshell.html'))
+        } else {
+            handle(req, res, parsedUrl)
+        }
+    }).listen(port)
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
+    // tslint:disable-next-line:no-console
+    console.log(
+        `> Server listening at http://localhost:${port} as ${
+            dev ? 'development' : process.env.NODE_ENV
+            }`
+    )
 })
